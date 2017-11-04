841618c09d381a691ba3696af4d0cc6f0dbf70c1
Michael Ashton <mykl.ashton@gmail.com>
2017-11-04 16:23:14 -0400

    Basic socket connected and rendering

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,17 @@
+import openSocket from 'socket.io-client';
+
 /**
  * Create a function that can be called to emit the subscribeToTimer event to 
  * the server and feed back the results of the timer event to the consuming code.
  */
 
- export function subscribeToTimer(interval, cb) {
+const socket = openSocket('http://localhost:8000'); // construct socket using socket.io's baked-in export method
 
- }
+
+export function subscribeToTimer(interval, cb) {
+  // vv subscribe to 'timer' event before emitting subscribeToTimer event to avoid a race between sever emitting events and client showing interest in those events, causing events to go missing
+  socket.on('timer', (timestamp) => cb(null, timestamp));
+  socket.emit('subscribeToTimer', interval)
+}
+
+// see how this function is imported and utilized in App.js
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+
+// ASSETS
+import { subscribeToTimer } from '../api/api';
 import '../styles/App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    subscribeToTimer( (err, timestamp) => this.setState({ timestamp }))
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  }
+
   render() {
     return (
       <div className='App'>
-          <h1>You're good to go! Now fix the internet...</h1>
+          <h1>This is a wicked socket example</h1>
+          <p> The current time is {this.state.timestamp} </p>
       </div>
     );
   }
