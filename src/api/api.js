177507e1f281eec4ef600208b4f3fb12cc062f81
Michael Ashton <mykl.ashton@gmail.com>
2017-11-04 16:02:55 -0400

    Begin setting up client side socket connection

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+/**
+ * Create a function that can be called to emit the subscribeToTimer event to 
+ * the server and feed back the results of the timer event to the consuming code.
+ */
+
+ export function subscribeToTimer(interval, cb) {
+
+ }
